1040454652e2b372019a69fdb3ce3555927933f1
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-26 22:02:01 +0530

    basic setup with a new branch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import RatingCard from "./components/ratingCard";
 function App() {
   return (
     <div>
       <h1 className="text-center text-[3rem] font-bold text-red-600">
         Card component
       </h1>
+      <RatingCard />
     </div>
   );
 }
---
+++ src/components/ratingCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RatingCard() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl text-red-700">
+        rating card component
+      </h1>
+    </div>
+  );
+}
+
+export default RatingCard;
