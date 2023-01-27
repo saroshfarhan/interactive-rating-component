82a997be474de3f3fdc8b62e343174de92df7a85
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-28 00:22:53 +0530

    rating component UI done

---
+++ src/components/completed.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Completed() {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-red-500">
+        Completed component
+      </h1>
+    </div>
+  );
+}
+
+export default Completed;
--- src/components/ratingCard.jsx
+++ src/components/ratingCard.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import star from "../assets/icon-star.svg";
 
 function RatingCard() {
+  const [rating, setRating] = useState(""); //need to lift the state up to parent
+
+  const r = [1, 2, 3, 4, 5];
+  //need to apply css to cehcked and unchecked button
+  const radio = r.map((rating) => {
+    return (
+      <button
+        key={rating}
+        className="h-12 w-12 rounded-full bg-mediumGrey hover:bg-orange"
+      >
+        {rating}
+      </button>
+    );
+  });
+
   return (
-    <div>
-      <h1 className="text-center text-3xl text-red-700">
-        rating card component
-      </h1>
+    <div
+      id="ratingCard"
+      className="w-[340px] rounded-2xl bg-darkBlue px-6 py-8 text-white"
+    >
+      <div
+        id="logoContainer"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-mediumGrey"
+      >
+        <img src={star} alt="star icon" />
+      </div>
+      <h1 className="mt-8 text-3xl font-[700]">How did we do?</h1>
+      <p className="mt-3 text-sm text-mediumGrey">
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div className="mt-8 flex justify-between">{radio}</div>
+      <button
+        type="submit"
+        className="mt-10 w-full rounded-full bg-orange py-3 text-center uppercase tracking-widest hover:bg-white hover:text-orange"
+      >
+        Submit
+      </button>
     </div>
   );
 }
